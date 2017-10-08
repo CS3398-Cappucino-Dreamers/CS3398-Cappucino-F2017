0086b0797f5fad0df5e6d9b6b6c22931f80e0382
TheAppFoundry <wbh26@txstate.edu>
2017-10-08 11:36:15 -0500

    Created Scrollable Page functionality on Login Activity
    Scrolls between a max of four fragments on the top half of the login activity

--- MyApplication/app/src/main/java/com/example/ben/fitordie/Login/LoginActivity.java
+++ MyApplication/app/src/main/java/com/example/ben/fitordie/Login/LoginActivity.java
@@ -2,6 +2,7 @@ package com.example.ben.fitordie.Login;
 
 import android.content.Intent;
 import android.nfc.Tag;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,11 +10,17 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.example.ben.fitordie.Login.SwipeViews.LoginFragmentPagerAdapter;
 import com.example.ben.fitordie.R;
 
+/**
+ * Allows users to login to the app or go to a sign-up activity. The top half of the activity
+ * is scrollable pages that allows users to discover more about our app. (info about it)
+ */
 public class LoginActivity extends AppCompatActivity {
 
-
+    private ViewPager mPager; // For swiping between fragments
+    private LoginFragmentPagerAdapter mAdapter; // represents each page as a Fragment
     private TextView signUpText; // Sign Up Text that will be clickable
 
     @Override
@@ -22,6 +29,13 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_fit_or_die);
 
         signUpText = (TextView) findViewById(R.id.signUp);
+
+        // Custom FragmentPagerAdapter is set to ViewPager to manage fragment instances
+        mAdapter = new LoginFragmentPagerAdapter(getSupportFragmentManager());
+        mPager = (ViewPager)findViewById(R.id.viewPager);
+        mPager.setAdapter(mAdapter);
+
+        setPageListener(); // sets the page scroll listener for the ViewPager
 
         initializeListeners();
 
@@ -50,5 +64,24 @@ public class LoginActivity extends AppCompatActivity {
 
     }
 
+    public void setPageListener(){
+        mPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
 
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                    Log.d("TAG", "onPageSelected: " + position);
+                    mPager.setCurrentItem(position);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+    }
 }
