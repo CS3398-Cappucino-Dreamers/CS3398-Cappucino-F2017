fdf4649c081fa7510693d8b37009cc5709308078
TheAppFoundry <wbh26@txstate.edu>
2017-10-08 15:58:19 -0500

    Created Dot Page Transitions and Fragment One content

--- MyApplication/app/src/main/java/com/example/ben/fitordie/Login/LoginActivity.java
+++ MyApplication/app/src/main/java/com/example/ben/fitordie/Login/LoginActivity.java
@@ -1,6 +1,10 @@
 package com.example.ben.fitordie.Login;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.LayerDrawable;
+import android.graphics.drawable.LevelListDrawable;
+import android.graphics.drawable.TransitionDrawable;
 import android.nfc.Tag;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
@@ -8,6 +12,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.ben.fitordie.Login.SwipeViews.LoginFragmentPagerAdapter;
@@ -23,6 +28,11 @@ public class LoginActivity extends AppCompatActivity {
     private LoginFragmentPagerAdapter mAdapter; // represents each page as a Fragment
     private TextView signUpText; // Sign Up Text that will be clickable
 
+
+
+    private LevelListDrawable layerDrawable; // contains all the dots Images
+    private ImageView dotsImg; // ImageView whose src is the levelListDrawable
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,8 +46,11 @@ public class LoginActivity extends AppCompatActivity {
         mPager.setAdapter(mAdapter);
 
         setPageListener(); // sets the page scroll listener for the ViewPager
+        initializeListeners(); // For views on the page
 
-        initializeListeners();
+        // dots that show you what page you are on
+        dotsImg = (ImageView)findViewById(R.id.dots);
+        layerDrawable = (LevelListDrawable) dotsImg.getDrawable();
 
     }
 
@@ -73,8 +86,9 @@ public class LoginActivity extends AppCompatActivity {
 
             @Override
             public void onPageSelected(int position) {
-                    Log.d("TAG", "onPageSelected: " + position);
-                    mPager.setCurrentItem(position);
+                Log.d("TAG", "onPageSelected: " + position);
+                mPager.setCurrentItem(position); // change fragment
+                layerDrawable.setLevel(position); // change dots image
             }
 
             @Override
