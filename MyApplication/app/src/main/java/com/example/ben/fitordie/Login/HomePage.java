3ebeb2417ea8e575da1ef0c3dfd1d10618492ec9
Ben Hunt <wbh26@txstate.edu>
2017-11-06 00:12:57 -0600

    Preliminary Navigation Drawer, Action Bar, and Progress Animation

--- MyApplication/app/src/main/java/com/example/ben/fitordie/Login/HomePage.java
+++ MyApplication/app/src/main/java/com/example/ben/fitordie/Login/HomePage.java
@@ -3,8 +3,13 @@ package com.example.ben.fitordie.Login;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.*;
+import android.view.Menu;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
@@ -16,25 +21,59 @@ import com.example.ben.fitordie.R;
 
 public class HomePage extends AppCompatActivity {
 
+    private CircleProgressBar circleProgressBar;
+    private Thread animation; // animation thread
     private TextView progressField;
     private Button calendarBtn;
     private Intent goToCalendarIntent;
     private AHBottomNavigationItem item1; // item1 of the bottom nav bar (Far left)
+    private ListView mDrawerList; // listview for the navigation drawer
+    private ArrayAdapter<String> mAdapter; // Adapts Data
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
 
-        BottomNavBar bottomNavBar = BottomNavBar.getInstance(this,findViewById(R.id.bottom_navigation));
+        mDrawerList = (ListView)findViewById(R.id.navList); // get Drawer ListView
+        addDrawerItems(); // populate drawer
+
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setTitleTextColor(getResources().getColor(R.color.lightGray));
+
+
+        BottomNavBar bottomNavBar = BottomNavBar.getInstance(this,findViewById(R. id.bottom_navigation));
 
         progressField = (TextView)findViewById(R.id.progressField);
         calendarBtn = (Button)findViewById(R.id.calendarBtn);
         setBtnListeners();
 
-        final CircleProgressBar circleProgressBar = (CircleProgressBar)findViewById(R.id.custom_progressBar);
+        circleProgressBar = (CircleProgressBar)findViewById(R.id.custom_progressBar);
         SeekBar seekBar = (SeekBar)findViewById(R.id.seekBar);
         goToCalendarIntent = new Intent(this, CalendarActivity.class);
+
+//        animation = new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                 runOnUiThread(new Runnable() {
+//                    @Override
+//                    public void run() {
+//                        for(int i =0; i< 700; i+=10){
+//                            circleProgressBar.setProgress(i/10);
+//                            try {
+//                                Thread.sleep(200);
+//                            } catch (InterruptedException e) {
+//                                e.printStackTrace();
+//                            }
+//                        }
+//                    }
+//                });
+//            }
+//        });
+//        animation.start();
+
 
 
 
@@ -58,15 +97,65 @@ public class HomePage extends AppCompatActivity {
         });
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
     /**
      * Button listener that takes you to the calenar activity.. Just temporary for demo #1
      */
-    public void setBtnListeners(){
+    public void setBtnListeners() {
         calendarBtn.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
-                startActivity(goToCalendarIntent);
+                //startActivity(goToCalendarIntent;
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        int b = 0;
+                        for(int i = 0; i < 70; i++) {
+                            final int a = i;
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    circleProgressBar.setProgress(a);
+                                }
+                            });
+                            try {
+
+                                Thread.currentThread().sleep(50);
+                                if(a >= 50){
+                                    Thread.currentThread().sleep(b+=5);
+                                }
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                }).start();
+
+
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.appbar,menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    private void addDrawerItems() {
+        String[] osArray = { "User Stats", "Calendar", "Tracker", "Logbook","Machine Learning", "Settings" };
+        mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, osArray);
+        mDrawerList.setAdapter(mAdapter);
     }
 }
