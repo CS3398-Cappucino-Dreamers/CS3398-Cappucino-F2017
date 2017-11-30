de7afd7017f8c3541f253e94de15bf8629228b44
lms251 <31562399+lms251@users.noreply.github.com>
2017-11-29 19:30:19 -0600

    updates

--- MyApplication/app/src/main/java/com/example/ben/fitordie/Login/Avatar.java
+++ MyApplication/app/src/main/java/com/example/ben/fitordie/Login/Avatar.java
@@ -2,11 +2,14 @@ package com.example.ben.fitordie.Login;
 
 import android.content.Intent;
 import android.graphics.Color;
+import android.graphics.drawable.LayerDrawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
@@ -25,24 +28,40 @@ import com.example.ben.fitordie.R;
 public class Avatar extends AppCompatActivity {
 
     private static final String TAG ="Avatar";
-    private Thread animation; // animation thread
-    private ProgressBar progressBar;
+    private Button customizeBtn;
+    public AvatarDrawable avatarDrawable;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.avatar_page);
 
+        customizeBtn = (Button)findViewById(R.id.customize);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         toolbar.setTitleTextColor(getResources().getColor(R.color.lightGray));
         toolbar.setOnMenuItemClickListener(new MenuItemListener(this));
 
+        customizeListener();
+
 
 
     }
+    public void goToCustomize(){
+        Intent intent = new Intent(this, Customize.class);
+        startActivity(intent);
+    }
 
-
+    public void customizeListener() {
+        customizeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToCustomize();
+            }
+        });
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.appbar,menu);
